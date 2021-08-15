4281b7084719b53816b62f5734379ddfc3569dea
karuulme <karuulmee@gmail.com>
2021-08-15 18:49:37 +0300

    sos yapımı

---
+++ SOS/SOS/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace SOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SiraliPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Puan1 = new System.Windows.Forms.Label();
+            this.Puan2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SiraliPanel
+            // 
+            this.SiraliPanel.Location = new System.Drawing.Point(2, 61);
+            this.SiraliPanel.Name = "SiraliPanel";
+            this.SiraliPanel.Size = new System.Drawing.Size(789, 811);
+            this.SiraliPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. OYUNCU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(581, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2. OYUNCU";
+            // 
+            // Puan1
+            // 
+            this.Puan1.AutoSize = true;
+            this.Puan1.Location = new System.Drawing.Point(118, 27);
+            this.Puan1.Name = "Puan1";
+            this.Puan1.Size = new System.Drawing.Size(0, 13);
+            this.Puan1.TabIndex = 3;
+            // 
+            // Puan2
+            // 
+            this.Puan2.AutoSize = true;
+            this.Puan2.Location = new System.Drawing.Point(697, 27);
+            this.Puan2.Name = "Puan2";
+            this.Puan2.Size = new System.Drawing.Size(0, 13);
+            this.Puan2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 888);
+            this.Controls.Add(this.Puan2);
+            this.Controls.Add(this.Puan1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SiraliPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel SiraliPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Puan1;
+        private System.Windows.Forms.Label Puan2;
+    }
+}
+
---
+++ SOS/SOS/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Button[,] Butonlar = new Button[14,14];
+        Panel[,] paneller = new Panel[14, 14];
+        int[,] Konumlar = new int[14, 14];
+        int[,] User1 = new int[14, 14];
+        int[,] User2 = new int[14, 14];
+        bool userControl1 = true;
+        bool userControl2 = false;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i=0;i<14;i++)
+            {
+                for( int j = 0; j < 14; j++)
+                {
+                Button btnS = new Button();
+                btnS.Width = 25;
+                btnS.Height = 50;
+                btnS.Text = "S";
+                btnS.Name = i.ToString() + "-" + j.ToString();
+                Button btnO = new Button();
+                btnO.Left = 25;
+                btnO.Width = 25;
+                btnO.Height = 50;
+                btnO.Text = "O";
+                btnO.Name = i.ToString() +"-"+ j.ToString();
+                paneller[i, j] = new Panel();
+                paneller[i, j].Width = 50;
+                paneller[i, j].Height = 50;
+                paneller[i, j].Controls.Add(btnS);
+                paneller[i, j].Controls.Add(btnO);
+                paneller[i, j].Name = i.ToString() +"-"+ j.ToString();
+                SiraliPanel.Controls.Add(paneller[i, j]);
+                btnS.Click += new EventHandler(KClick);
+                btnO.Click += new EventHandler(KClick);
+            }
+            }
+
+        }
+        public void KClick(object sender, EventArgs e)
+        {
+            Button buton = (Button)sender;
+            string numara = buton.Name.ToString();
+           
+           // string[] parcalar=numara.Split('-');
+           // MessageBox.Show(parcalar[0]);
+            for (int i = 0; i < 14; i++)
+            {
+                for (int j = 0; j < 14; j++)
+                {
+                    if (paneller[i, j].Name.ToString()==numara)
+                    {
+                        paneller[i, j].Controls.Clear();
+                        if (userControl1==true)
+                        {
+                            userControl1 = false;
+                            userControl2 = true;
+                            Butonlar[i,j] = new Button();
+                            Butonlar[i, j].Width = 50;
+                            Butonlar[i, j].Font = new Font(Butonlar[i, j].Font.FontFamily, 30);
+                            Butonlar[i, j].Height = 50;
+                            Butonlar[i, j].BackColor = Color.Green;
+                            Butonlar[i, j].Text = buton.Text;
+                            paneller[i, j].Controls.Add(Butonlar[i, j]);
+                            if (Butonlar[i, j].Text == "S")
+                            {
+
+                                User1[i, j] = 1;
+                                DiziKontrol(User1, 1,1);
+                            }
+                            else
+                            {
+                                User1[i, j] = 2;
+                                DiziKontrol(User1, 2,1);
+                            }
+                            
+                           
+                        }
+                        else
+                        {
+                            userControl1 = true;
+                            userControl2 = false;
+                            Button butonn = new Button();
+                            butonn.Width = 50;
+                            butonn.Font = new Font(butonn.Font.FontFamily, 30);
+                            butonn.Height = 50;
+                            butonn.BackColor = Color.Red;
+                            butonn.Text = buton.Text;
+                            paneller[i, j].Controls.Add(butonn);
+                            if (butonn.Text == "S")
+                            {
+
+                                User2[i, j] = 1;
+                                DiziKontrol(User2,1,2);
+                            }
+                            else
+                            {
+                                User2[i, j] = 2;
+                                DiziKontrol(User2, 2, 2);
+
+                            }
+                           
+                        }
+                    }
+
+
+                }
+            }
+
+
+
+
+        }
+
+
+        public void DiziKontrol(int[,] Dizi,int T,int User)
+        {
+
+            for (int i=0;i<14;i++)
+            {
+                for (int j = 0; j < 14; j++)
+                {
+
+                  
+                    if (Dizi[i, j]==1 || Dizi[i, j]==2) {
+
+                       
+
+                        for (int h = i-1; h <i+2; h++)
+                            {
+                                for (int r = j-1; r < j+2; r++)
+                                {
+                                if (Dizi[i, j]==1)
+                                {
+                                    if(Butonlar[h, r]==2)
+                                    
+
+
+                                }
+                                paneller[h, r].BackColor=Color.Black;
+
+
+                                 }
+
+                        }
+
+
+
+
+
+
+
+
+                     
+
+
+                    }
+
+
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+}
